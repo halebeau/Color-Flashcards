c70377a07b073ddba044998ee0899284c8c63103
Beau Hale <halebeau3@gmail.com>
2020-01-22 14:37:04 -0800

    Functionality

--- src/App.js
+++ src/App.js
@@ -1,56 +1,64 @@
 import React, { Component } from "react";
 import "./App.css";
 import { data } from "./data/data";
-import Card from "./components/Card"
+import Card from "./components/Card";
 
 const colorsToPick = [
   {
-    label: 'blue',
-    color: '#40BBFF',
+    label: "blue",
+    color: "#40BBFF"
   },
-  { 
-    label: 'green', 
-  color: '#50E3C2'
-},
-  { 
-    label: 'pink', 
-  color: '#DF70B5'
-},
-  { 
-    label: 'red', 
-  color: '#FF6A85'
-},
-  { 
-    label: 'orange', 
-  color: '#FEB102',
-}
-]
+  {
+    label: "green",
+    color: "#50E3C2"
+  },
+  {
+    label: "pink",
+    color: "#DF70B5"
+  },
+  {
+    label: "red",
+    color: "#FF6A85"
+  },
+  {
+    label: "orange",
+    color: "#FEB102"
+  }
+];
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       data: data,
-      title: '',
-      description: '',
-      selectedColor: 'blue',
-      show: false,
-    }
+      title: "",
+      description: "",
+      selectedColor: "blue",
+      show: false
+    };
 
     //bind click
     this.createNewCard = this.createNewCard.bind(this);
   }
 
   checkSelectedColor(color) {
-    this.setState({selectedColor: color})
+    this.setState({ selectedColor: color });
   }
 
   updateTitle(title) {
-    this.setState({title})
+    //check the state to show the remaining form
+    if (title.length > 0) {
+      this.setState({ show: true });
+    } else {
+      this.setState({show: false})
+    }
+
+    //  update text
+    this.setState({ title });
   }
 
   updateDescription(description) {
-    this.setState({description})
+    this.setState({ description });
   }
 
   createNewCard() {
@@ -58,13 +66,13 @@ class App extends Component {
       title: this.state.title,
       date: Date.now(),
       description: this.state.description,
-      color: this.state.selectedColor,
-    }
+      color: this.state.selectedColor
+    };
 
     // add it to the state
     let newData = this.state.data;
-    newData = [ newCard,...newData]
-    this.setState({data: newData})
+    newData = [newCard, ...newData];
+    this.setState({ data: newData });
   }
 
   render() {
@@ -74,51 +82,49 @@ class App extends Component {
           <input
             className="box input-title"
             type="text"
-            placeholder='Title'
-            onChange={ (event) => this.updateTitle(event.target.value) }
+            placeholder="Title"
+            onChange={event => this.updateTitle(event.target.value)}
           />
-          
-          <textarea
-            className="box input-description"
-            type="text"
-            placeholder="Description"
-            onChange={(event) => this.updateDescription(event.target.value)}
-          >
-          
-          </textarea>
-          <p>
-            {
-              colorsToPick.map((color, index) => {
-                const selectedClass = (this.state.selectedColor === color.label)
-                  ? `color-picker select ${color.label}` : 'color-picker';
-                
+
+          <div style={{display: this.state.show ? 'block' : 'none'}}>
+            <textarea
+              className="box input-description"
+              type="text"
+              placeholder="Description"
+              onChange={event => this.updateDescription(event.target.value)}
+            ></textarea>
+            <p>
+              {colorsToPick.map((color, index) => {
+                const selectedClass =
+                  this.state.selectedColor === color.label
+                    ? `color-picker select ${color.label}`
+                    : "color-picker";
+
                 return (
-                  <a key={ index } onClick={() => this.checkSelectedColor(color.label)} >
-                    <span className={selectedClass}
-                      style={ {backgroundColor: color.color }}
+                  <a
+                    key={index}
+                    onClick={() => this.checkSelectedColor(color.label)}
+                  >
+                    <span
+                      className={selectedClass}
+                      style={{ backgroundColor: color.color }}
                     ></span>
                   </a>
-                )
-              })
-            }
-          </p>
-          <button className="create-button" onClick={this.createNewCard}>
-            Create New
-            </button>
+                );
+              })}
+              <button className="create-button" onClick={this.createNewCard}>
+                Create New
+              </button>
+            </p>
+          </div>
         </div>
 
-        {
-          this.state.data.map((node) => {
-            return (
-              <Card key={ node.date } node={ node } color={ node.color } />
-        )
-      })
-    }
-          </div>
-
-
-);
-}
+        {this.state.data.map(node => {
+          return <Card key={node.date} node={node} color={node.color} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
